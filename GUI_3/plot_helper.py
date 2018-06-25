3a40daf35bc0dcaf21e050ddc5d9a00e6c2de0bf
Tianxing <tj258@cornell.edu>
2018-06-25 09:15:47 -0400

    Add a helper function for plotting

---
+++ GUI_3/plot_helper.py
@@ -0,0 +1,20 @@
+import matplotlib as plt
+
+
+class PlotHelper(object):
+    """
+    Class to help plot the graph, with prev and next button on the tk canvas
+    """
+    def __init__(self, canvas, file_list):
+        pass
+
+    def save_file(self, path, extension, size):
+        """
+        Method to save images in the given directory
+
+        :param path: String, The directory to save in
+        :param extension: String, The file format, e.g. png, svg
+        :param size: List or tuple, The size of the image, e.g. (800, 600)
+        :return: None
+        """
+        pass
